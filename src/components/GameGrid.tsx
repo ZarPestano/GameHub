a30b809ba33bbf95b3a29227eb961a820fa0d541
Zar Pestano <ezarzai@hotmail.com>
2024-08-19 10:48:41 -0700

    Refactor: Extract a query object

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -7,14 +7,14 @@ import GameCardContainer from "./GameCardContainer";
 import useGames, { Platform } from "../hooks/useGames";
 
 import { Genre } from "../hooks/useGenres";
+import { GameQuery } from "../App";
 
 interface Props {
-  selectedGenre: Genre | null;
-  selectedPlatform: Platform | null;
+  gameQuery: GameQuery;
 }
 
-const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
-  const { data, error, isLoading } = useGames(selectedGenre, selectedPlatform);
+const GameGrid = ({ gameQuery }: Props) => {
+  const { data, error, isLoading } = useGames(gameQuery);
 
   const skeletons = [1, 2, 3, 4, 5, 6];
 
