1275078ee952b501db533e2da7a7abcd8d4a64ce
Eleazar Pestano <eleazar.pestano@hotmail.com>
2024-10-04 13:38:16 -0700

    Fetching games using React Query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "35cc5a0ead024b80ba4ad9bc58a9cc57"
-  }
+    key: "35cc5a0ead024b80ba4ad9bc58a9cc57",
+  },
 });
